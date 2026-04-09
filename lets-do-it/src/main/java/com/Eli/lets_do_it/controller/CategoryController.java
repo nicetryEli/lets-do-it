5145621959abe20467665ba290754659f7205f54
nicetryEli <annalilly131205@gmail.com>
2026-04-09 20:25:02 +0700

    updated on 09/04/2026

--- lets-do-it/src/main/java/com/Eli/lets_do_it/controller/CategoryController.java
+++ lets-do-it/src/main/java/com/Eli/lets_do_it/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.Eli.lets_do_it.controller;
 
 import com.Eli.lets_do_it.model.Category;
+import com.Eli.lets_do_it.model.Task;
 import com.Eli.lets_do_it.service.interfaces.CategoryService;
 import com.Eli.lets_do_it.service.interfaces.UserService;
 import jakarta.persistence.GeneratedValue;
@@ -18,6 +19,21 @@ public class CategoryController {
     @GetMapping
     public List<Category> getAllCategory(){return categoryService.getAllCategory();}
 
+    @GetMapping("/{id}")
+    public List<Task> getAllTask(@RequestBody Category category){
+        return categoryService.getAllTaskFromCategory(category);
+    }
+
     @PostMapping
     public Category createCategory(@RequestBody Category category){return categoryService.createCategory(category);}
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Integer id, @RequestBody Category category){
+        return categoryService.updateCategory(id,category);
+    }
+
+    @DeleteMapping("/{id}")
+    public Category deleteCategory(@PathVariable Integer id){
+        return categoryService.deleteCategory(id);
+    }
 }
